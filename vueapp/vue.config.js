a1362d284a89fc19b5a37468f393ce6e9354ad4a
nikolappc <neekolapapic@gmail.com>
2020-05-22 14:23:10 +0200

    Pokusaj netlify host-a

--- vueapp/vue.config.js
+++ vueapp/vue.config.js
@@ -4,7 +4,9 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-
+  devServer: {
+    proxy: 'https://mrsisa-tim06.herokuapp.com/'
+  },
   outputDir: path.resolve(__dirname,'..\\SpringWebApp\\src\\main\\resources\\static'),
   publicPath:""
 }
